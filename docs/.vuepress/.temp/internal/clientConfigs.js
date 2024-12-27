ed9bf206a625ed3f65c03ead8c0236113815c274
conglongping <1010578148@qq.com>
2024-12-27 12:14:28 +0800

    feat: 初始化

---
+++ docs/.vuepress/.temp/internal/clientConfigs.js
@@ -0,0 +1,23 @@
+import * as clientConfig0 from 'G:/react/前端编码规范工程化/demo/node_modules/.pnpm/@vuepress+plugin-active-header-links@2.0.0-rc.66_typescript@5.7.2_vuepress@2.0.0-rc.19_@vuepr_rxmjywcc2f4a3jqe3r6vsfneni/node_modules/@vuepress/plugin-active-header-links/lib/client/config.js'
+import * as clientConfig1 from 'G:/react/前端编码规范工程化/demo/node_modules/.pnpm/@vuepress+plugin-back-to-top@2.0.0-rc.66_typescript@5.7.2_vuepress@2.0.0-rc.19_@vuepress+bund_hq7nxxak3dtm627hkattaucxnu/node_modules/@vuepress/plugin-back-to-top/lib/client/config.js'
+import * as clientConfig2 from 'G:/react/前端编码规范工程化/demo/node_modules/.pnpm/@vuepress+plugin-copy-code@2.0.0-rc.66_typescript@5.7.2_vuepress@2.0.0-rc.19_@vuepress+bundle_4oqfd7wm2wwukgnn2ajrqaykga/node_modules/@vuepress/plugin-copy-code/lib/client/config.js'
+import * as clientConfig3 from 'G:/react/前端编码规范工程化/demo/node_modules/.pnpm/@vuepress+plugin-markdown-hint@2.0.0-rc.66_markdown-it@14.1.0_typescript@5.7.2_vuepress@2.0.0_trn5f732c7iflzrimcq5l5acem/node_modules/@vuepress/plugin-markdown-hint/lib/client/config.js'
+import * as clientConfig4 from 'G:/react/前端编码规范工程化/demo/node_modules/.pnpm/@vuepress+plugin-medium-zoom@2.0.0-rc.66_typescript@5.7.2_vuepress@2.0.0-rc.19_@vuepress+bund_73xpbcrtleg7p33epwewqyh3bm/node_modules/@vuepress/plugin-medium-zoom/lib/client/config.js'
+import * as clientConfig5 from 'G:/react/前端编码规范工程化/demo/node_modules/.pnpm/@vuepress+plugin-nprogress@2.0.0-rc.66_typescript@5.7.2_vuepress@2.0.0-rc.19_@vuepress+bundle_ui552yh63odush6f2nwvd6gh2m/node_modules/@vuepress/plugin-nprogress/lib/client/config.js'
+import * as clientConfig6 from 'G:/react/前端编码规范工程化/demo/docs/.vuepress/.temp/prismjs/config.js'
+import * as clientConfig7 from 'G:/react/前端编码规范工程化/demo/docs/.vuepress/.temp/markdown-tab/config.js'
+import * as clientConfig8 from 'G:/react/前端编码规范工程化/demo/node_modules/.pnpm/@vuepress+plugin-theme-data@2.0.0-rc.66_typescript@5.7.2_vuepress@2.0.0-rc.19_@vuepress+bundl_3rl6jaen46lhy4lyhxnlh72u4i/node_modules/@vuepress/plugin-theme-data/lib/client/config.js'
+import * as clientConfig9 from 'G:/react/前端编码规范工程化/demo/node_modules/.pnpm/@vuepress+theme-default@2.0.0-rc.66_markdown-it@14.1.0_sass-embedded@1.83.0_typescript@5.7.2__hg7txdozsjf4pyw5esbe4vhbt4/node_modules/@vuepress/theme-default/lib/client/config.js'
+
+export const clientConfigs = [
+  clientConfig0,
+  clientConfig1,
+  clientConfig2,
+  clientConfig3,
+  clientConfig4,
+  clientConfig5,
+  clientConfig6,
+  clientConfig7,
+  clientConfig8,
+  clientConfig9,
+].map((m) => m.default).filter(Boolean)
---
+++ docs/.vuepress/.temp/prismjs/config.js
@@ -0,0 +1,12 @@
+import "G:/react/前端编码规范工程化/demo/node_modules/.pnpm/@vuepress+highlighter-helper@2.0.0-rc.66_@vueuse+core@12.2.0_typescript@5.7.2__vuepress@2.0.0_qwp6jesqmrfgfe3f2rydnwel6y/node_modules/@vuepress/highlighter-helper/lib/client/styles/base.css"
+import "G:/react/前端编码规范工程化/demo/node_modules/.pnpm/@vuepress+plugin-prismjs@2.0.0-rc.66_@vueuse+core@12.2.0_typescript@5.7.2__typescript@5.7.2_v_podo2w2c6mnyp7v367kt3mjnne/node_modules/@vuepress/plugin-prismjs/lib/client/styles/nord.css"
+import "G:/react/前端编码规范工程化/demo/node_modules/.pnpm/@vuepress+highlighter-helper@2.0.0-rc.66_@vueuse+core@12.2.0_typescript@5.7.2__vuepress@2.0.0_qwp6jesqmrfgfe3f2rydnwel6y/node_modules/@vuepress/highlighter-helper/lib/client/styles/line-numbers.css"
+import "G:/react/前端编码规范工程化/demo/node_modules/.pnpm/@vuepress+highlighter-helper@2.0.0-rc.66_@vueuse+core@12.2.0_typescript@5.7.2__vuepress@2.0.0_qwp6jesqmrfgfe3f2rydnwel6y/node_modules/@vuepress/highlighter-helper/lib/client/styles/notation-highlight.css"
+import "G:/react/前端编码规范工程化/demo/node_modules/.pnpm/@vuepress+highlighter-helper@2.0.0-rc.66_@vueuse+core@12.2.0_typescript@5.7.2__vuepress@2.0.0_qwp6jesqmrfgfe3f2rydnwel6y/node_modules/@vuepress/highlighter-helper/lib/client/styles/collapsed-lines.css"
+import { setupCollapsedLines } from "G:/react/前端编码规范工程化/demo/node_modules/.pnpm/@vuepress+highlighter-helper@2.0.0-rc.66_@vueuse+core@12.2.0_typescript@5.7.2__vuepress@2.0.0_qwp6jesqmrfgfe3f2rydnwel6y/node_modules/@vuepress/highlighter-helper/lib/client/index.js"
+
+export default {
+  setup() {
+    setupCollapsedLines()
+  }
+}
